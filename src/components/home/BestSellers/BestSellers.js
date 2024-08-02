bd46a27cd2fcc06124e27cb9616e2b3c0b5189fc
muttaisammy <muttaisammy@gmail.com>
2024-08-02 10:30:03 +0300

    m

--- src/components/home/BestSellers/BestSellers.js
+++ src/components/home/BestSellers/BestSellers.js
@@ -11,9 +11,10 @@ import {
 
 function BestSellers() {
   return (
-    <div className="w-full pb-20">
+    <div className="w-full pb-20;">
       <Heading heading="Top Deals on Market" />
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 lgl:grid-cols-3 xl:grid-cols-4 gap-10">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lgl:grid-cols-3 height: 100% xl:grid-cols-4 gap-10; "
+      >
         <Product
           _id="1011"
           img={bestSellerOne}
